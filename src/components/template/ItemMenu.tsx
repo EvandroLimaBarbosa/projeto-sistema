1ce77c920650200972da807f36f8d6ddbc8c870d
Evandro Lima <evandrolimabarbosa@hotmail.com>
2024-02-13 18:56:28 -0300

    Alterando botão de saida

--- src/components/template/ItemMenu.tsx
+++ src/components/template/ItemMenu.tsx
@@ -1,25 +1,39 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-  url: string;
   text: string;
   icon: any;
+  url?: string;
+  onClick?: (evento: any) => void;
+  className?: string;
 }
 
 export default function ItemMenu(props: MenuItemProps) {
+  const renderLink = () => {
+    return <a className={`
+    flex flex-col justify-center items-center 
+    w-20 h-20 text-gray-600 ${props.className}
+  `} href={props.url}>
+      {props.icon}
+      <span className={`
+    text-xs font-light
+    `}>
+        {props.text}
+      </span>
+    </a>
+  }
   return (
-    <li className={`hover:bg-gray-100`}>
-      <Link className={`
-          flex flex-col justify-center items-center 
-          w-20 h-20
-        `} href={props.url}>
-        {props.icon}
-        <span className={`
-          text-xs font-light text-gray-600
-          `}>
-          {props.text}
-        </span>
-      </Link>
+    <li onClick={props.onClick} className={`
+    hover:bg-gray-100
+    cursor-pointer
+      `}>
+      {props.url ? (
+        <Link legacyBehavior href={props.url}>
+          {renderLink()}
+        </Link>
+      ) : (
+        renderLink()
+      )}
     </li>
   )
 }
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -4,20 +4,22 @@ import Logo from "./Logo";
 
 export default function SideMenu() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div className={`
         flex flex-col items-center justify-center
         bg-gradient-to-r from-indigo-500 to-purple-800
         h-20 w-20
       `}>
-        <Logo/>
+        <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <ItemMenu url="/" text="Início" icon={HomeIcon} />
         <ItemMenu url="/agenda" text="Agenda" icon={CalendarIcon} />
-        <ItemMenu url="/configuracao" text="Configuração" icon={SettingsIcon} />
         <ItemMenu url="/logo" text="Logo" icon={ComputerIcon} />
-        <ItemMenu url="/Sair" text="Sair" icon={ExitIcon} />
+      </ul>
+      <ul className="">
+        <ItemMenu url="/configuracao" text="Opções" icon={SettingsIcon} />
+        <ItemMenu className={`text-red-600 hover:bg-red-600 hover:text-white`} onClick={() => console.log('saiu')} text="Sair" icon={ExitIcon} />
       </ul>
     </aside>
   )
