fbede24591b9b568d1ce77fcc3ba162df1eca0a6
Evandro Lima <evandrolimabarbosa@hotmail.com>
2024-03-29 12:08:03 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,11 +6,6 @@ if (!firebase.apps.length) {
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-
-        // apiKey:'AIzaSyD63VEw_O_t0pmZT186GD9HyuPOTACKOWo',
-        // authDomain:'admin-template-1.firebaseapp.com',
-        // projectId:'admin-template-1'
-
     })
 }
 
