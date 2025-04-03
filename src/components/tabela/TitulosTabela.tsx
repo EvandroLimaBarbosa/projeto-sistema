29f381a0e666d8af250c34098e21a89389226b53
EvandroLimaBarbosa <evandrolimabarbosa@hotmail.com>
2025-04-03 00:26:22 -0300

    Remove unnecessary rounded corners from TitulosTabela component for cleaner design

--- src/components/tabela/TitulosTabela.tsx
+++ src/components/tabela/TitulosTabela.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material'
 
 export default function TitulosTabela() {
     return (
-        <Box className={`flex flex-row bg-gradient-to-r from-blue-950 via-blue-950 to-blue-900 text-white h-10 items-center rounded-t-md`}>
+        <Box className={`flex flex-row bg-gradient-to-r from-blue-950 via-blue-950 to-blue-900 text-white h-10 items-center`}>
             <Typography variant="body1" className='flex justify-center w-24'> Horário </Typography>
             <Typography variant="body1" className='flex justify-center w-28'> Nome </Typography>
             <Typography variant="body1" className='flex justify-center w-40'> Serviço </Typography>
